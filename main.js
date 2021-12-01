8bb42cfa082f2ab5cda8286aba1741a515e9a540
MohammadJouza <m.jozaa@yahoo.com>
2021-12-01 12:12:38 +0300

    finish   STEP 2: When the player click on one of the cell => Show X || O

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
-console.log("Week 4 - Day 4 | Project - 1 [11:33 AM] ");
+console.log("Week 4 - Day 4 | Project - 1 [12:08 PM] ");
 
 $(document).ready(function () {
+  const cells = $(".cell");
+  const turnSpan = $("#turn");
+  // console.log(cells);
+  let turn = "X";
+  const O = "O";
+  const X = "X";
+
+  cells.click(cellClicked);
+
+  function cellClicked() {
+    // console.log(this);
+    // this => DOM
+    if ($(this).text() === "") {
+      if (turn === X) {
+        $(this).text(X);
+        turnSpan.text(O);
+        turn = O;
+      } else {
+        $(this).text(O);
+        turnSpan.text(X);
+        turn = X;
+      }
+    } else {
+      $(this).css("background-color", "red");
+      setTimeout(()=>{
+        $(this).css("background-color", "white");
+      },250)
+    }
+  }
+});
+
 /*
   A. Grid Layout [HTML + CSS]
     HTML: use div with id 'board' and 9 div with class 'cell'
@@ -14,12 +45,15 @@ $(document).ready(function () {
     CSS: p text-align: center;    
 
   B. When the player click on one of the cell => Show X || O
-    Extra: show the turn to which player => Show X || O
+    show click X
+    show click X || O (turn variable to switch between turns)
+    Show the turn show turn to which player => PLAYER TURN: X || O
+    Extra: 
+      Invalid move => background red || can't click 
+      
+  NOW. when click on the button 'Play Again' the game will restart or the page will refresh
 
-  C. After the player click on one of the cell => Switch player
+  C. When the player select 3 Win
 
-  D. When the player select 3 Win
-
-  E. Show who win and a button to "Play Again"
+  D. Show who win and a button to "Play Again"
 */
-});
