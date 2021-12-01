c0413aab5613015066b46f728c5988a404b65997
MohammadJouza <m.jozaa@yahoo.com>
2021-12-01 11:36:12 +0300

    finish STEP 1: Grid Layout [HTML + CSS]

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-console.log("Week 4 - Day 4 | Project - 1 [11:06 AM] ");
+console.log("Week 4 - Day 4 | Project - 1 [11:33 AM] ");
 
 $(document).ready(function () {
+/*
+  A. Grid Layout [HTML + CSS]
+    HTML: use div with id 'board' and 9 div with class 'cell'
+    CSS: style grid "board"
+    CSS: style width & height "cell"
+    HTML: add div id="all"
+    HTML: add par "PLAYER TURN:" id='turn'
+    HTML: add par "WIN" id='winner'
+    HTML: add button "Play Again" id='restart'
+    CSS: #all display: grid; && justify-content: center;
+    CSS: p text-align: center;    
 
-  // 1. Grid Layout [HTML + CSS]
-  // use div with id 'board' and 9 div with class 'cell'
+  B. When the player click on one of the cell => Show X || O
+    Extra: show the turn to which player => Show X || O
 
-  // 2. When the player click on one of the cell => Show X || O
+  C. After the player click on one of the cell => Switch player
 
-  // 3. After the player click on one of the cell => Switch player
+  D. When the player select 3 Win
 
-  // 4. When the player select 3 Win
-
-  // 5. Show who win and a button to "Play Again"
-
+  E. Show who win and a button to "Play Again"
+*/
 });
