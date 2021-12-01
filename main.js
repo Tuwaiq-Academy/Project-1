caede987851147e01afaa6a4a96112f92f832e50
MohammadJouza <m.jozaa@yahoo.com>
2021-12-01 11:14:51 +0300

    step 0

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("Week 4 - Day 4 | Project - 1 [11:06 AM] ");
+
+$(document).ready(function () {
+
+  // 1. Grid Layout [HTML + CSS]
+  // use div with id 'board' and 9 div with class 'cell'
+
+  // 2. When the player click on one of the cell => Show X || O
+
+  // 3. After the player click on one of the cell => Switch player
+
+  // 4. When the player select 3 Win
+
+  // 5. Show who win and a button to "Play Again"
+
+});
